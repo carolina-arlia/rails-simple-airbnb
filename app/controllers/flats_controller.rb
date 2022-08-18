2071589d92ec9493b912433d2e1a67daaa54d3aa
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-18 15:00:21 +0200

    wip

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,27 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: "Flat was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
 end
